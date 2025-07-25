67a7a26e24876ba4b1e00d582a73327d8076d574
shivkant-epam <shivkantkr9934@gmail.com>
2025-07-25 10:47:21 +0530

    Driver initilization + adding singletonDesign Pattern

---
+++ src/main/java/org/finSecure/driver/Singleton/DriverManager.java
@@ -0,0 +1,35 @@
+package org.finSecure.driver.Singleton;
+
+import org.finSecure.driver.factory.DriverFactory;
+import org.finSecure.driver.manager.Browser;
+import org.openqa.selenium.WebDriver;
+
+
+public class DriverManager {
+
+        private static WebDriver driver;
+
+        private DriverManager() {
+            // private constructor to prevent external instantiation
+        }
+
+        public static WebDriver getDriver(String browserType) {
+            if (driver == null) {
+                synchronized (DriverManager.class) {
+                    if (driver == null) {
+                        Browser browser = DriverFactory.getDriver(browserType);
+                        driver = browser.createDriver();
+                    }
+                }
+            }
+            return driver;
+        }
+
+        public static void quitDriver() {
+            if (driver != null) {
+                driver.quit();
+                driver = null;
+            }
+        }
+    }
+
